36b31447ed844a60a9f9c1685ad8378290f1683e
grumancik <genrumancik@gmail.com>
2013-10-10 18:02:53 -0700

    first commit

---
+++ strings.js
@@ -0,0 +1,38 @@
+var myName = "Gen Rumancik";
+
+var output = "";
+
+for (var i = 11; i >= 0; i -=1) {
+switch (myName[i]) {
+	case "a":
+	case "e":
+	case "i":
+	case "o":
+	case "u":
+	break;
+	default:	
+
+	output = output + myName[i]
+  }
+}
+console.log(output)
+
+var Dino = "Tyrannasaurus Rex"
+
+var outpu = "";
+
+for (var i = 16; i >= 0; i -=1) {
+	switch (Dino[i]) {
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "u":
+		break;
+		default:
+
+		outpu = outpu + Dino[i]
+
+	}
+}
+	console.log(outpu)
